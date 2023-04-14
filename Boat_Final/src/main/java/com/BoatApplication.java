6e71f1586d9df6aaed2181573c7a0d1d10fee35f
izzy215 <jihyeon3@gmail.com>
2023-04-14 12:47:40 +0900

    2.

--- Boat_Final/src/main/java/com/BoatApplication.java
+++ Boat_Final/src/main/java/com/BoatApplication.java
@@ -2,7 +2,7 @@ package com;
 //localhost:9700/boat/index
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+  
 @SpringBootApplication
 public class BoatApplication {
 
